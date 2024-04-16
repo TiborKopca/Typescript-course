e67ef9b6ad82f608b0ea9d44e9cee768cdb5447e
TiborKopca <tibor.kopca@gmail.com>
2024-04-16 12:01:37 +0200

    generics2

--- src/generics.ts
+++ src/generics.ts
@@ -74,11 +74,112 @@ let arrayNumbers = createArray<number>(4, 100); // [100, 100, 100, 100]
 console.log(arrayStrings); //(3) ['hello', 'hello', 'hello']
 console.log(arrayNumbers); //(4) [100, 100, 100, 100]
 
-//Multiple variable types in generic function
-function pair<T, U>(param1: T, param2: U): [T, U] {
+//Multiple variable types in generic function, i've added union type syntax---------------------//
+function pair<T, U, V>(param1: T, param2: U | V): [T, U | V] {
   return [param1, param2];
 }
 
-// Usage
-let result2 = pair<number, string>(123, "Hello");
+//Usage
+let result2 = pair<number, string, boolean>(123, "Hello");
 console.log(result2); // Output: [123, "Hello"]
+let result3 = pair<number, string, boolean>(456, true);
+console.log(result3); // Output: [456, true]
+
+//Inferred Type -- Typescript can function like this, but we need to be carefull to pass the correct values
+let result4 = pair(0, "This works too, but carefull with values");
+console.log(result4); // Output: [0, This works too, but carefull with values]
+//  const [name,setName] = useState('')
+//  const [products,setProducts] = useState<Product[]>([])
+
+//Type Constraints--------------------------------------------//
+//type constraint on the generic type T, generic type can be either a number or a string.
+function processValue<T extends number | string>(value: T): T {
+  console.log(value);
+  return value;
+}
+
+//the same function can handle string or number, but no boolean in this case
+processValue("hello"); //hello
+processValue(12); //12
+// processValue(true);     //true - it will be logged out but typescript shows us this is not correct
+
+//More Type Constraints--------------------------------------------//
+type Product = {
+  name: string;
+  price: number;
+};
+
+const product: Product = {
+  name: "shoes",
+  price: 1.99,
+};
+
+type Student = {
+  name: string;
+  age: number;
+};
+
+const student: Student = {
+  name: "peter",
+  age: 20,
+};
+
+// T extends Student is a type constraint on the generic type T. It means that the type T can be any type, but it must be a subtype of Student or Student itself. In other words, T must have at least the same properties and methods that Student has.
+function printName<T extends Student>(input: T): void {
+  console.log(input.name);
+}
+printName(student); //peter
+
+function printNameUnion<T extends Student | Product>(input: T): void {
+  console.log(input.name);
+}
+printNameUnion(product); //shoes
+
+//The extends "any object" { name: string } part is a type constraint on T.
+//It means that T can be any type, but it must be an object that has at least a name property of type string.
+//In other words, T must have at least the same properties and methods that { name: string } has.
+function printName2<T extends { name: string }>(input: T): void {
+  console.log(input.name);
+}
+printName2(student); //peter
+printName2(product); //shoes
+
+//Generics - Default Value --> set the interface by default to some type-----------------------//
+interface StoreData<T = any> {
+  data: T[];
+}
+
+const storeNumbers: StoreData<number> = {
+  data: [1, 2, 3, 4],
+};
+console.log(storeNumbers) //{data: Array(4)} data: [1, 2, 3, 4]
+
+//Store data has no type provided, because we set it by default above
+const randomStuff: StoreData = {
+  data: ["random", 1],
+};
+console.log(randomStuff) //{data: Array(2)}data: ["random", 1]
+
+
+//AXIOS - HTTP REQUESTS
+//data is located in the data property of the object
+// let someUrl = 'https://volando.art'
+// const { data } = axios.get(someUrl);
+
+// axios.get<{ name: string }[]>(someUrl);
+
+// export class Axios {
+//   get<T = any, R = AxiosResponse<T>, D = any>(
+//     url: string,
+//     config?: AxiosRequestConfig<D>
+//   ): Promise<R>;
+// }
+
+// export interface AxiosResponse<T = any, D = any> {
+//   data: T;
+//   status: number;
+//   statusText: string;
+//   headers: RawAxiosResponseHeaders | AxiosResponseHeaders;
+//   config: InternalAxiosRequestConfig<D>;
+//   request?: any;
+// }
